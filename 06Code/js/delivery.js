53d25c96b7bcc80a9ddf660f9e1c470ab5d5ea5b
Luciana Noemi Guerra López <75289552+LucianaGuerra0@users.noreply.github.com>
2023-03-03 00:35:13 -0500

    Update

--- 06Code/js/delivery.js
+++ 06Code/js/delivery.js
@@ -1,25 +1,75 @@
-function validarFormulario() {
-    // Obtener referencias a los campos del formulario
-    var zip = document.getElementById("zip");
+function validateHTML() {
+  // Obtener referencias a los campos del formulario
+  const name = document.getElementById("name").value;
+  const lastname = document.getElementById("lastname").value;
+  const city = document.getElementById('city');
+  const inputAddress2 = document.getElementById("inputAddress2").value;
+  const zip = document.getElementById("zip").value;
+  const phone = document.getElementById("phone").value;
+  const description = document.getElementById("description").value;
+  const Card= document.querySelector('input[name="Card"]:checked');
   
-    // Obtener referencias a los elementos de mensaje de error
 
-    var errorZip = document.getElementById("error-zip");
-  
-    // Bandera para indicar si el formulario es válido o no
-    var formularioValido = true;
-  
-   
-  
-  // Validar el campo de código postal
-  if (zip.value.length < 6) {
-    errorZip.innerHTML = "Please enter a valid zip code";
-    formularioValido = false;
+  // Validar el campo de nombre
+  if (name == '') {
+    document.getElementById('name-error').innerHTML = '<span style="color:red">Please enter your name.</span>';
+    return false;
   } else {
-    errorZip.innerHTML = "";
+    document.getElementById('name-error').innerHTML = '';
   }
-  
-  // Devolver si el formulario es válido o no
-  return formularioValido;
+
+  // Validar el campo de apellido
+  if (lastname == '') {
+    document.getElementById('lastname-error').innerHTML = '<span style="color:red">Please enter your lastname.</span>';
+    return false;
+  } else {
+    document.getElementById("lastname-error").innerHTML = '';
   }
-  
+
+  //validar el campo 
+  if (city == '') {
+    document.getElementById('city-error').innerHTML = '<span style="color:red">Please select your city.</span>';
+    return false;
+  } else {
+    document.getElementById("city-error").innerHTML = '';
+  }
+
+  // Validar el campo inputAddress2
+  if (inputAddress2 == '') {
+    document.getElementById('inputAddress2-error').innerHTML = '<span style="color:red">Please enter your adreess.</span>';
+    return false;
+  } else {
+    document.getElementById('inputAddress2-error').innerHTML = '';
+  }
+
+  // Validar el campo de zip
+  if (!zip) {
+    document.getElementById('zip-error').innerHTML = '<span style="color:red">Please enter your zip.</span>';
+    return false;
+  } else {
+    document.getElementById('zip-error').innerHTML = '';
+  }
+
+  // Validar el campo de phone
+  if (!phone) {
+    document.getElementById('phone-error').innerHTML = '<span style="color:red">Please enter your phone.</span>';
+    return false;
+  } else {
+    document.getElementById('phone-error').innerHTML = '';
+  }
+
+  // Validar el campo de dresscription
+  if (description == '') {
+    document.getElementById('description-error').innerHTML = '<span style="color:red">Please enter your description.</span>';
+    return false;
+  } else {
+    document.getElementById("description-error").innerHTML = '';
+  }
+// Validar el campo de tarjeta
+if (!Card) {
+  document.getElementById('card-error').innerHTML = '<span style="color:red">Please select one.</span>';
+  return false;
+} else {
+  document.getElementById('card-error').innerHTML = '';
+}
+}
